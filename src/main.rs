a97b22a2518e218dc7ac0237ea8b5ddf2e94aa93
Geometer1729 <16kuhnb@gmail.com>
2024-10-19 19:54:49 -0400

    better image library

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod trig;
 mod proj;
 
-extern crate bmp;
+extern crate image;
 
 use std::f64::consts::TAU;
+use image::{imageops::interpolate_bilinear, open, RgbImage};
 use proj::project;
 
 use clap::Parser;
-use bmp::{Image, Pixel};
 
 use rayon::prelude::*;
 
@@ -46,36 +46,31 @@ fn main() {
     }
 
     let map_name = args.map.unwrap_or("testMap.bmp".to_string());
-    let source_map = bmp::open(map_name).expect("failed to open");
+    let source_map = open(map_name).expect("failed to open").into_rgb8();
     let long = args.long.unwrap_or(0.0) * TAU/360.0;
     let lat = (args.lat.unwrap_or(0.0) + 90.0) * TAU/360.0;
-    let width = args.width.unwrap_or(256);
-    let height = args.height.unwrap_or(256);
-    let scale = args.scale.unwrap_or(1.0);
+    let width = args.width.unwrap_or(512);
+    let height = args.height.unwrap_or(512);
+    let scale = args.scale.unwrap_or(400.0);
 
-    let mut img = Image::new(width, height);
-    let forward_map : Vec<(u32,u32,(f64,f64))> =
-        img.coordinates()
-            .collect::<Vec<_>>()
-            .par_iter()
-            .map(|&(x,y)|(x,y,pixel_for
-                (source_map.get_height() as f64
-                ,source_map.get_width() as f64
-                ,scale
-                ,width
-                ,height
-                ,lat
-                ,long
-                ,x
-                ,y
-            )))
-            .collect();
-    for &(x,y,(px,py)) in forward_map.iter() {
-        let pixel = source_map.get_pixel(px as u32,py as u32);
-        img.set_pixel(x,y,pixel);
-    }
-    let _ = img.save("img.bmp");
-
+    let mut img = RgbImage::new(width, height);
+    img.par_enumerate_pixels_mut()
+            .for_each(|(x,y,p)|
+                { let (px,py) =
+                    pixel_for
+                    (source_map.height() as f64
+                    ,source_map.width() as f64
+                    ,scale
+                    ,width
+                    ,height
+                    ,lat
+                    ,long
+                    ,x
+                    ,y
+                    );
+                *p = interpolate_bilinear(&source_map,px as f32,py as f32).unwrap();
+            });
+    img.save("img.jpg").unwrap();
 }
 
 const TARGET_STEP : f64 = 100.0;
@@ -93,8 +88,8 @@ fn pixel_for
     let x1 = (x as f64 - (width/2) as f64)*scale as f64;
     let y1 = (y as f64 - (height/2) as f64)*scale as f64;
     let [x2,y2] = project([long,lat],[x1,y1],TARGET_STEP);
-    let x3 = (x2 + TAU/2.0)/TAU*source_w;
-    let y3 = (y2 + TAU/2.0)/TAU*source_h;
+    let x3 = (x2 + TAU/2.0)/TAU*(source_w-1.0);
+    let y3 = (y2 + TAU/2.0)/TAU*(source_h-1.0);
     (x3,y3)
 }
 
